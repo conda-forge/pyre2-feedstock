788ba827ca9eded19e26fe8e8f4321e6a891ed24
Stephen L Arnold <nerdboy@gentoo.org>
2021-01-21 10:56:46 -0800

    remove bogus sysroot arg (specified in compiler-tools.html#macos-sdk)
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,9 +9,5 @@ export CFLAGS=$(echo "${CFLAGS}" | sed "s/-O2/-O3/g")
 export CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-O2/-O3/g")
 export LDFLAGS=$(echo "${LDFLAGS}" | sed "s/-O2/-O3/g")
 
-if [[ $(uname) == Darwin ]]; then
-    export CFLAGS="${CFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
-    export CXXFLAGS="${CXXFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
-fi
 
 $PREFIX/bin/python -m pip install . -vv
